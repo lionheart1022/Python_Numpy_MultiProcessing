dd0b63f010e881f5fd7a9e49a9c2952a230f7b23
Oleksii Zamiatin <alex.zamiatin910@gmail.com>
2018-11-05 17:34:03 +0200

    update api output as json format and config

--- api.py
+++ api.py
@@ -32,9 +32,19 @@ def display(text):
     length = len(keywords)
 
     for i in range(1, length+1):
-        result.update({'TOP CONTEXT {}'.format(i): str(keywords[i-1])})
+        for key in keywords[i-1]:
+            result_value = {}
+            for sub_key in keywords[i-1][key]:
+                sub_key_value = {"keyword_location": [str(keywords[i-1][key][sub_key]['keyword_location'][0])],
+                                 "keyword_text": str(keywords[i-1][key][sub_key]['keyword_text']),
+                                 "keyword_phrase_id": str(keywords[i-1][key][sub_key]['keyword_phrase_id'])}
+                result_value.update({str(sub_key): sub_key_value})
+
+            formatted_value = {str(key): result_value}
+            result.update({'TOP CONTEXT {}'.format(i): formatted_value})
+
     return jsonify(result)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5300')
+    app.run(host='0.0.0.0', port='5302')
